2e02379f35eeb6063094c949c32d45cd57a28d71
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-18 14:13:55 +0300

    Add validation to user resource.
    Download class-validator and class-transformer
    and validate user resource in create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,24 @@
+import {
+  IsAlphanumeric,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  @IsAlphanumeric()
   name: string;
+
+  @IsInt()
+  @Min(18)
+  @Max(100)
   age: number;
 }
