08b104a141e41580bd5a89fa1c9b7023f2f62991
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-20 20:23:53 +0300

    Move ConfigModule to it is own file in config folder.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,30 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { EnvironmentVariables } from './config/config.dto';
-import { plainToInstance } from 'class-transformer';
-import { validateSync } from 'class-validator';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validate: (config) => {
-        const validatedConfig = plainToInstance(EnvironmentVariables, config, {
-          enableImplicitConversion: true,
-        });
-        const errors = validateSync(validatedConfig);
-        if (errors.length > 0) {
-          throw new Error(errors.toString());
-        }
-        return validatedConfig;
-      },
-    }),
-    DatabaseModule,
-    UserModule,
-    AuthModule,
-  ],
+  imports: [ConfigModule, DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
+import { EnvironmentVariables } from './config.dto';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => {
+        const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+          enableImplicitConversion: true,
+        });
+        const errors = validateSync(validatedConfig);
+        if (errors.length > 0) {
+          throw new Error(errors.toString());
+        }
+        return validatedConfig;
+      },
+    }),
+  ],
+  exports: [NestConfigModule],
+})
+export class ConfigModule {}
