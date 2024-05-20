d52158329327846ae62db84414a3b14e9d27b74f
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-20 23:54:27 +0300

    Update auth guards.

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -4,9 +4,9 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 import { ConfigVariables } from 'src/config/config.interface';
 
 @Injectable()
@@ -20,7 +20,9 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
-    if (!token) throw new UnauthorizedException();
+    if (!token) {
+      throw new UnauthorizedException('Missing or invalid access token');
+    }
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -29,13 +31,13 @@ export class AuthGuard implements CanActivate {
 
       request.user = payload;
     } catch (err) {
-      throw new UnauthorizedException(err);
+      throw new UnauthorizedException('Invalid token');
     }
 
     return true;
   }
 
-  private extractTokenFromHeader(request: Request) {
+  private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
 
     return type === 'Bearer' ? token : undefined;
--- src/auth/guards/refresh-jwt.guard.ts
+++ src/auth/guards/refresh-jwt.guard.ts
@@ -4,9 +4,9 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
 import { ConfigVariables } from 'src/config/config.interface';
 
 @Injectable()
@@ -20,7 +20,9 @@ export class RefreshJwtGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
-    if (!token) throw new UnauthorizedException();
+    if (!token) {
+      throw new UnauthorizedException('Missing refresh token');
+    }
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -29,7 +31,7 @@ export class RefreshJwtGuard implements CanActivate {
 
       request.user = payload;
     } catch (err) {
-      throw new UnauthorizedException(err);
+      throw new UnauthorizedException('Missing or invalid refresh token');
     }
 
     return true;
