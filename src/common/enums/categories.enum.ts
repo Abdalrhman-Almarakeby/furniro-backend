20bfcb1f76511ce1ff6e1d42963374998ef1b286
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-05-24 17:09:34 +0300

    Create categories enum.

---
+++ src/common/enums/categories.enum.ts
@@ -0,0 +1,8 @@
+export enum Categories {
+  LIVING_ROOM = 'Living Room',
+  BEDROOM = 'Bedroom',
+  KITCHEN = 'Kitchen',
+  BATHROOM = 'Bathroom',
+  DINING_ROOM = 'Dining Room',
+  OUTDOOR = 'Outdoor',
+}
