2afc7145efeb45b1cf255e259c0cf85820f9d6a4
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-13 04:24:16 +0300

    Update HttpsRedirectMiddleware middleware.

--- src/common/middlewares/https-redirect.middleware.ts
+++ src/common/middlewares/https-redirect.middleware.ts
@@ -4,7 +4,16 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class HttpsRedirectMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    const isLocalhost =
+      req.hostname === 'localhost' ||
+      req.hostname === '127.0.0.1' ||
+      req.hostname.startsWith('192.168.');
+
+    if (
+      !isLocalhost &&
+      !req.secure &&
+      req.get('x-forwarded-proto') !== 'https'
+    ) {
       return res.redirect(`https://${req.headers.host}${req.url}`);
     }
 
